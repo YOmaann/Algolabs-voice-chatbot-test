cca3550fc4ea1c85637fac8e4f47b9fcd7dcbff2
creationisme <srishtilakhotia1@gmail.com>
2025-06-16 21:37:32 +0530

    minor changes to embeddocs

--- lib/text_text/embedding/embeddocs.py
+++ lib/text_text/embedding/embeddocs.py
@@ -1,23 +1,30 @@
-# embed different documents in different collections, in same directory (? is this right)
-
+# embed different documents in different collections, in same directory 
+# can rewrite without Langchain wrapper: a little faster generally
 from langchain_chroma import Chroma
 from langchain_huggingface import HuggingFaceEmbeddings
-from langchain.text_splitter import RecursiveCharacterTextSplitter #use AITextSplitter instead
-from .agentic_chunker.agentic_chunker import LLMChunker
-from langchain.schema import Document 
+#from langchain.text_splitter import RecursiveCharacterTextSplitter 
+from .agentic_chunker.agentic_chunker import LLMChunker # can modify LLM chunker to support langchain Documents
+#from langchain.schema import Document 
+import time
 
 embedfn = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2") #can vary model name
 
 content = []
 with open("pencilwiki.txt","r",encoding="utf-8") as file: 
     content.append(file.read(20))
-    content.append(file.read(20))
 
-docs = [Document(page_content=doc) for doc in content]
-splitter = RecursiveCharacterTextSplitter(chunk_size=5, chunk_overlap=2) #can try to vary params
-splitter2 = LLMChunker()
-chunks = splitter.split_documents(documents=docs) #use split_text if splitter2
-vectorstore = Chroma.from_documents(chunks, collection_name="mycoll", embedding=embedfn, persist_directory="db") 
-#Chroma.add_documents for adding documents
-#can have different collections in same directory
+splitter = LLMChunker()
 
+start = time.time()
+chunks = splitter.split_text(content)
+time_taken = time.time()-start
+
+print(f"Time taken by LLM Chunker: {time_taken} seconds")
+
+start2 = time.time()
+vectorstore = Chroma.from_texts(chunks, collection_name="coll", embedding=embedfn, persist_directory="db") #keep better collection_name and add metadata for ease by LLM
+time_taken2 = time.time() - start2
+print(f"Time taken for embedding using model: {embedfn}:\n{time_taken2} seconds")
+
+#recommended to add metadata document to each collection explaining what it contains for ease of access to the AI Agent later and then integrate the metadata into its prompt
+#^ not coded up yet
